1845519c9be3a975438d7d5174331f3cb8d91cc7
Dizbdeedee <dizbee@mentice.cotse.net>
2021-01-11 22:21:45 +0000

    Improved consistency for exporting
    FossilOrigin-Name: fbb07e07e52e0c8990f7f5fe84ba6b35d2759aa5932a964aa1d2b40e27688dd7

--- src/gmod/macros/InitMacro.hx
+++ src/gmod/macros/InitMacro.hx
@@ -84,7 +84,7 @@ class InitMacro {
 local haxeEnv = {}
 local _hx_exports = {}
 _G.HAXE_$addonName = haxeEnv 
-setmetatable(_hx_exports,{__newindex = _G})
+setmetatable(_hx_exports,{__index = _G,__newindex = _G})
 setmetatable(haxeEnv,{__index = _G})
 setfenv(1,haxeEnv) --if using more than one project + dce, global collisions and missing indexes will ensue. dont want that 
 '); //TODO move to template system
