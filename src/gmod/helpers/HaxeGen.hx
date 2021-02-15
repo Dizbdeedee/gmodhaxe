70f01a4eb78b9ef6b9611740ea281abeeb9ba22e
Dizbdeedee <dizbee@mentice.cotse.net>
2021-02-15 20:38:30 +0000

    Readd haxegen
    FossilOrigin-Name: 9c1c9a870ba13b6bbe3fb0877b3f4d047769ce60a1df40223f0de8026062d09c

---
+++ src/gmod/helpers/HaxeGen.hx
@@ -0,0 +1,29 @@
+package gmod.helpers;
+
+typedef HaxeExtend<T,X:HaxeExtend<T,X>> = {
+    final self:HaxeGen<T,X>;
+}
+/**
+    Internally used by generated ENTS
+**/
+@:forward
+abstract HaxeGen<ENT,HAXE:HaxeExtend<ENT,HAXE>>(ENT) from ENT to ENT {
+
+    public var haxeGen(get,never):HAXE;
+    
+    @:noCompletion
+    extern inline function get_haxeGen():HAXE {
+        return untyped this._gHaxeBurrow;
+    }
+
+    @:to
+    public extern inline function toHaxe():HAXE {
+        return untyped this._gHaxeBurrow;
+    }
+
+    @:from
+    public inline static function fromHaxe<ENT,HAXE:HaxeExtend<ENT,HAXE>>(x:HAXE):HaxeGen<ENT,HAXE> {
+        return x.self;
+    }
+
+}
