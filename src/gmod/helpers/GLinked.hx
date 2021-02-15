b4cd0e64277e5e58d567e808c3f98a202cfee773
Dizbdeedee <dizbee@mentice.cotse.net>
2021-02-15 23:56:12 +0000

    HaxeGen renamed to GLinked
    FossilOrigin-Name: 8418e92fecb3304205b4598a07cabbc492d40af035b5826f2f4bc32eaaf16429

---
+++ src/gmod/helpers/GLinked.hx
@@ -0,0 +1,29 @@
+package gmod.helpers;
+typedef HLinked<G,H:HLinked<G,H>> = {
+    final self:GLinked<G,H>;
+}
+/**
+    A gmod object containing a custom haxe class equivalent
+    If you create a custom entity using SentBuild, you will be able to get the haxe class equivalent from this
+**/
+@:forward
+abstract GLinked<GBASE,HAXE:HLinked<GBASE,HAXE>>(GBASE) from GBASE to GBASE {
+
+    public var linkedHaxe(get,never):HAXE;
+    
+    @:noCompletion
+    extern inline function get_linkedHaxe():HAXE {
+        return untyped this._gHaxeBurrow;
+    }
+
+    @:to
+    public extern inline function toHaxe():HAXE {
+        return untyped this._gHaxeBurrow;
+    }
+
+    @:from
+    public inline static function fromHaxe<GBASE,HAXE:HLinked<GBASE,HAXE>>(x:HAXE):GLinked<GBASE,HAXE> {
+        return x.self;
+    }
+
+}
