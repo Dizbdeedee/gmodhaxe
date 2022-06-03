efc2e66cef82410ef4e8be54dc1694a695db8167
Dizbdeedee <dizbee@mentice.cotse.net>
2022-06-03 12:43:29 +0000

    Weaktools was lost, and now it is found
    FossilOrigin-Name: d823634edfa6ff4e071380038941689c8cb6137503b7c8526658474e811de59d

--- src/gmod/helpers/WeakTools.hx
+++ src/gmod/helpers/WeakTools.hx
@@ -4,32 +4,35 @@ package gmod.helpers;
 @:access(haxe.ds.ObjectMap)
 class WeakTools {
 
-    public static function setWeakKeys(table:lua.Table.AnyTable):Void {
+	@:noUsing
+	static function createMTIfNotExists(table:lua.Table.AnyTable) {
 		var prevMeta:lua.Table.AnyTable = Gmod.getmetatable(table);
 		if (prevMeta == null) {
 			prevMeta = Table.create();
 			Gmod.setmetatable(table,prevMeta);
-		} 
-		prevMeta.__mode = "k";
+		}
+		return prevMeta;
+	}
+	
+    public static function setWeakKeys(table:lua.Table.AnyTable):Void {
+		final meta = createMTIfNotExists(table)
+		meta.__mode = "k";
     }
   
     public static function setWeakValues(table:lua.Table.AnyTable):Void {
-		var prevMeta:lua.Table.AnyTable = Gmod.getmetatable(table);
-		if (prevMeta == null) {
-			prevMeta = Table.create();
-			Gmod.setmetatable(table,prevMeta);
-		} 
-		prevMeta.__mode = "v";
+		final meta = createMTIfNotExists(table)
+		meta.__mode = "v";
     }
 
     public static function setWeakKeyValues(table:lua.Table.AnyTable):Void {
-		var prevMeta:lua.Table.AnyTable = Gmod.getmetatable(table);
-		if (prevMeta == null) {
-			prevMeta = Table.create();
-			Gmod.setmetatable(table,prevMeta);
-		} 
-		prevMeta.__mode = "kv";
+		final meta = createMTIfNotExists(table)
+		meta.__mode = "kv";
     }
+
+	public static function setGCMethod(table:lua.Table.AnyTable, meth:Dynamic) {
+		final meta = createMTIfNotExists(table)
+		meta.__gc = meth;
+	}
 
 
     public static function setWeakKeysM(objMap:haxe.ds.ObjectMap<Dynamic,Dynamic>) {
@@ -45,8 +48,6 @@ class WeakTools {
 		setWeakKeyValues(objMap.h);
 		setWeakKeys(objMap.k);
     }
-
-	
 
 	public static function setWeakValuesArr(arr:Array<Dynamic>) {
 		setWeakValues(cast arr);
